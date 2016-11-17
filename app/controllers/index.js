d9b7d9399010f7115daee432dcd70bac98fa80c5
James Ahlm <14782828428@qq.com>
2016-11-17 18:41:59 +0800

    Organized into modules

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,89 +1,5 @@
 
-//I'll probably need a larger Margin on the bottom for the x axis that will come out
-var margins = {
-"top" = 5,
-"left" = 5,
-"right" = 5, 
-"bottom" = 5
-}
 
-//It would be great if these values were exported
-var starterStocks = ["AAPL", "GOOG"]
-
-var width = 1000 - margins.left - margins.right
-var height = 500 - margins.top - margins.bottom
-
-var apiKey = "KUytvHxeZzXQzvSzJkrC"
-var ticker = "AAPL"
-var startDate = "20160912"
-var endDate = "20161002"
-var url = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=" + ticker + "&start_date=" + startDate + "&end_date=" + endDate + "&api_key=" + apiKey
-
-//d3
-
-//This call should eventually go into the React Section
-d3.json(url, function(data){
-data = data.datatable;
-drawGraph(data)
-})
-
-var drawG = function(data, stockCode){
-d3.select('#chart')
-.selectAll("g")
-.data(data)
-.enter()
-.attr("class", stockCode)
-.attr("transform", function(d){return "translate(" + d.date + "," + d.price + ")"})
-}
-
-var drawCircle = function(stockCode){
-d3.selectAll("g ." + stockCode)
-.append("circle")
-.attr("r", 2)
-.style("fill", "black")
-.style("stroke", "black")
-.style("stroke-width", "1px")
-}
-
-//Don't forget to return this
-var drawGraph = function(data){
-
-var dateData = data.map(function(d){
-return d[1]
-})
-var openData = data.map(function(d){
-return d[2]
-})
-var data = = data.map(function(d){
-return {"date": d[1], "price": d[2]}
-})
-
-//Get Maximum and Minimum Values for Scales
-
-var maxPrice = d3.max(openData);
-var minPrice = d3.min(openData);
-//FIX
-var minDate = new Date(dateData[0]);
-var maxDate = new Date("2016-11-15");
-
-var xScale = d3.scaleLinear().domain([minDate, maxDate]).range([0, width])
-var xAxis = d3.axisBottom().scale(xScale).tickSize(0)
-d3.select("chart").append("g").attr("id", "xAxisG").attr("transform", "translate(0," + height + ")").call(xAxis);
-
-//I'll just do share in US Dollars
-//So, I'll have to do dynamic resizing
-var yScale = d3.scaleLinear().domain([0, maxPrice]).range([0, height])
-var yAxis = d3.axisRight().scale(yScale).tickSize(0)
-d3.select("chart").append("g").attr("id", "yAxisG").attr("transform", "translate(-10,0)").call(yAxis)
-
-return data
-}
-
-
-
-
-//REACT
-//This container also handles drawing at the top of the page
 var WidgetContainer = React.createClass({
 getInitialState(){
 return {stocks: this.props.stocks, maxPrice: 0}
@@ -94,12 +10,24 @@ d3.select("chart")
 .attr("height". this.props.height);
 //Draw from the starterStocks
 //Check for highest Price and Update a marker
-for (
+for(var i = 0; i < this.props.length; i++){
+url = makeUrl(this.props.stocks[i], this.props.startDate, this.props.endDate, this.props.apiKey)
+d3.json(url, function(data){
+data = data.datatable;
+//Now What Needs to Happen is that the x and y axis need to get pulled into their own methods
+var dataDict = cleanData(data)
+drawScales(dataDict)
+})
+}
 
 },
 render(){
 //Return a ticker Widget for each starterStock
-return <TickerWidget /> <SelectorWidget />
+return (
+<div>
+<TickerWidget /> <SelectorWidget />
+</div>
+)
 }
 })
 
@@ -134,6 +62,6 @@ return(
 
 //Lets put the props in here
 ReactDOM.render(
-<WidgetContainer className="widget-container" url={url} width={width} height={height} stocks={starterStocks}/>,
+<WidgetContainer className="widget-container" apiKey={apiKey} startDate={defaultStartDate} endDate={defaultEndDate} url={url} width={width} height={height} stocks={starterStocks}/>,
 document.getElementById('content')
 )
